ae5e8df6c27eed34d2ac53d6f4e694685c09d992
siisak <au517946@post.au.dk>
2022-10-21 18:19:00 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const btn = document.getElementById("btn-a");
+const infobox = document.getElementById("info-box-a");
+btn.addEventListener("click", () => {
+    console.log("the text")
+  infobox.classList.toggle("hidden");
+});
+
+const btnb = document.getElementById("btn-b");
+const infoboxb = document.getElementById("info-box-b");
+btnb.addEventListener("click", () => {
+    console.log("the text")
+  infoboxb.classList.toggle("hidden");
+});
+
+const btnc = document.getElementById("btn-c");
+const infoboxc = document.getElementById("info-box-c");
+btnc.addEventListener("click", () => {
+    console.log("the text")
+  infoboxc.classList.toggle("hidden");
+});
+
+const btnd = document.getElementById("btn-d");
+const infoboxd = document.getElementById("info-box-d");
+btnd.addEventListener("click", () => {
+    console.log("the text")
+  infoboxd.classList.toggle("hidden");
+});
